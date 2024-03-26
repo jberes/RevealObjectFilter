14bdc691e9084806c12c630625834a271c8c7b8f
Jason Beres <jasonberes@gmail.com>
2024-03-26 17:54:18 -0400

    Add files via upload

---
+++ net-core/Reveal/DataSourceProvider.cs
@@ -0,0 +1,36 @@
+﻿using DocumentFormat.OpenXml.Drawing.Diagrams;
+using Reveal.Sdk;
+using Reveal.Sdk.Data;
+using Reveal.Sdk.Data.Microsoft.SqlServer;
+
+namespace RevealSdk.Server.Reveal
+{
+    internal class DataSourceProvider : IRVDataSourceProvider
+    {
+        public Task<RVDataSourceItem> ChangeDataSourceItemAsync
+                (IRVUserContext userContext, string dashboardId, RVDataSourceItem dataSourceItem)
+        {
+            if (dataSourceItem is RVSqlServerDataSourceItem sqlDsi)
+            {
+                ChangeDataSourceAsync(userContext, sqlDsi.DataSource);
+
+
+                sqlDsi.CustomQuery = (sqlDsi.Table == "All Orders" || sqlDsi.Table == "All Invoices") ?
+                    (userContext.Properties["Role"] == "Admin" ? "Select * from [" + sqlDsi.Table + "]" :
+                    "Select * from [" + sqlDsi.Table + "] where customerId = '" + userContext.UserId + "'") : null;
+
+            }
+            return Task.FromResult(dataSourceItem);
+        }
+
+        public Task<RVDashboardDataSource> ChangeDataSourceAsync(IRVUserContext userContext, RVDashboardDataSource dataSource)
+        {
+            if (dataSource is RVAzureSqlDataSource sqlDs)
+            {
+                sqlDs.Host = "database.windows.net";
+                sqlDs.Database = "Northwind";
+            }
+            return Task.FromResult(dataSource);
+        }
+    }
+}
